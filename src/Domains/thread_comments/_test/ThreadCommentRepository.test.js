c81f9691ce748fcd471770acf9cd32ecc3c965af
entity015 <83643128+entity015@users.noreply.github.com>
2023-10-19 09:04:26 +0700

    Typo: domain comments repository, domain thread comments repository

--- src/Domains/thread_comments/_test/ThreadCommentRepository.test.js
+++ src/Domains/thread_comments/_test/ThreadCommentRepository.test.js
@@ -6,7 +6,7 @@ describe("ThreadCommentRepository interface", () => {
 		const threadCommentRepository = new ThreadCommentRepository()
 
 		// Action & Assert
-		await expect(threadCommentRepository.addEntry("")).rejects.toThrowError("COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED")
-		await expect(threadCommentRepository.deleteEntry("")).rejects.toThrowError("COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED")
+		await expect(threadCommentRepository.addEntry("")).rejects.toThrowError("THREAD_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED")
+		await expect(threadCommentRepository.deleteEntry("")).rejects.toThrowError("THREAD_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED")
 	})
 })
